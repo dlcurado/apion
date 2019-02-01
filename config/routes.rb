34236fff1e86728252a062d8a8169702f955691c
Daniel Lourenço Curado <dlcurado@gmail.com>
2019-02-01 16:41:22 -0200

    Adionado um controlador de versiao da api e a classe validadora do cabecalho, que valida se a opcao de versao foi informada

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -6,6 +8,9 @@ Rails.application.routes.draw do
   # a nossa API dessa forma:
   # api.apion.com/[model]
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1) do
+      #resources :tasks
+    end
   end
 
 end
