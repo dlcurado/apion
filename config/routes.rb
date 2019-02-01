163fc1b87f1d7e9f3433170ae077b7560dd20124
Daniel Lourenço Curado <dlcurado@gmail.com>
2019-02-01 15:18:40 -0200

    Configurado o namespace no arquivos de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # constraints { subdomain: 'api' } => Substitui o www. por api. no comeco da URL
+  # path: "/" => tira a necessidade de identificar o /api apos a URL completa, deixando a URL para 
+  # a nossa API dessa forma:
+  # api.apion.com/[model]
+  namespace :api, defaults: { format: :json }, constraints { subdomain: 'api' }, path: "/" do
+  end
+
 end
