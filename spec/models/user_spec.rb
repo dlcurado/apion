438c203da5a1c0c8372ef2ebaf1ecf3c4b4358a0
Daniel Lourenço Curado <dlcurado@gmail.com>
2019-02-04 21:59:39 -0200

    Criado exemplos de criacao de testes com o rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
-  before { @user = FactoryGirl.build(:user) }
+  # pending "add some examples to (or delete) #{__FILE__}"
+  
+  # Convencional, instanciando um objeto do tipo user
+  # before { @user = FactoryGirl.build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+  #it { expect(@user).to respond_to(:email) }
+  #it { expect(@user).to respond_to(:name) }
+  #it { expect(@user).to respond_to(:password) }
+  #it { expect(@user).to respond_to(:password_confirmation) }
+  #it { expect(@user).to be_valid }
+
+  # Utilizando Subject
+  #subject { build(:user) }
+  #it { expect(subject).to respond_to(:email) }
+  #it { expect(subject).to be_valid }
+
+
+  # Outra forma seria referenciando direto o parametros User
+  it { is_expected. to respond_to(:email) }
 end
