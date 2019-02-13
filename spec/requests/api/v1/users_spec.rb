747d9109d078df3e6606cc7a1e3a500538bae2a2
Daniel Lourenço Curado <dlcurado@gmail.com>
2019-02-13 16:59:56 -0200

    Finalizados os testes de user (update e delete)

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -103,4 +103,19 @@ RSpec.describe 'Users API request', type: :request do
     
     end
 
+    describe 'DELETE /users/:id' do
+        before do
+            headers = { "Accept" => "application/vnd.apion.v1" }
+            delete "/users/#{user_id}", params: {}, headers: headers
+        end
+
+        it 'return status code 204' do
+            expect(response).to have_http_status(204)
+        end
+        
+        it 'removes the user from database' do
+           expect( User.find_by(id: user.id) ).to be_nil
+        end
+    end
+
 end
